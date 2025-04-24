3812b7bebd8dab58a2b4e9da743c7e939d24a7ec
joy zhang <joy.xyz@berkeley.edu>
2025-04-24 16:11:26 -0700

    myfridge add button and list save

--- src/MyFridge.js
+++ src/MyFridge.js
@@ -1,20 +1,64 @@
-import react from 'react'
+import react, { useState } from 'react'
 import './index.css'
+
 function MyFridge(){
+    const [itemName, setItemName] = useState('');
+    const [quantity, setQuantity] = useState(1);
+    const [fridgeItems, setFridgeItems] = useState([]);
+
+    const handleAddItem = () => {
+        if (itemName.trim() === '') return;
+
+        const newItem = {
+            name: itemName,
+            quantity: quantity,
+            id: Date.now()
+        };
+
+        setFridgeItems([...fridgeItems, newItem]);
+        setItemName('');
+        setQuantity(1);
+    }
+
+    const handleRemoveItem = (idexToRemove) => {
+        const updatedItems = (fridgeItems.filter((_, index) => index !== idexToRemove));
+        setFridgeItems(updatedItems);
+    }
+
     return (
         <div>
         <div className='green_header'>
             <button className='clear_fridge back_to_home'>Back to home</button>
             <h1>My Fridge</h1>
-            <button className='clear_fridge'> Clear Fridge</button>
+            <button className='clear_fridge' onClick={() => setFridgeItems([])}> Clear Fridge </button>
         </div>
 
-        <div>
-            <input className='input_food_items' type="text" placeholder="Enter food item..." />
+        <div style={{ margin: '2rem'}}>
+            <input className='input_food_items' type="text" placeholder="Enter food item..." value={itemName} onChange={(e) => setItemName(e.target.value)}
+            style={{ marginLeft: '10px', padding: '0.5rem'}}
+            />
+            <input type="number" min="1" value={quantity} onChange={(e) => setQuantity(Number(e.target.value))}
+            style={{ marginLeft: '10px', padding: '0.5rem', width: '100px' }}
+            />
+
+            <button onClick={handleAddItem} className='clear_fridge' style={{ marginLeft: '10px', padding: '0.5rem' }}>Add Item     
+            </button>
         </div>
 
-
-        </div>
+        <ul>
+            {fridgeItems.map((item, index) => (
+                <li key={index}> 
+                {item.quantity} {item.name}
+                <button
+                style ={{ marginLeft: '15px' }}
+                onClick={() => handleRemoveItem(index)}
+                >
+                    Remove Items
+                </button>
+                </li>
+            ))}
+        </ul>
+    </div>
 
 
     );
